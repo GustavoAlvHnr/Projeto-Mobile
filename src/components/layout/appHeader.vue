c440796ea48355ebda52db0868f89214cd22aead
Gianlucagadotti <gianlucagadotti@gmail.com>
2026-03-12 22:06:24 -0300

    Feat 4: Reestruturação do layout, criação da estrutura de rotas, criando appHeader e estilização de botões e placeholders

---
+++ src/components/layout/appHeader.vue
@@ -0,0 +1,16 @@
+<template>
+     <div class="header">
+            <h1>Controle de Gastos Rapido</h1>
+
+        </div>
+</template>
+
+<style scoped>
+
+h1{
+
+    color: black;
+    padding: 2px 6px;
+    margin: 0;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,15 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue'),
     },
+    {
+      path: '/Despesa-View',
+      name: 'expense',
+      component: () => import('../views/DespesaView.vue'),
+    }
   ],
 })
 
